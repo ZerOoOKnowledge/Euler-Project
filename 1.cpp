cc9767d824da698023d9fb2abb9a68773668a363
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-09-30 20:37:34 +0800

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+
+const int dr[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int dc[] = {0, -1, 0, 1, -1, 1, -1, 1};
+// Head
+
+int sum;
+
+int main() {
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) sum += i;
+    }
+
+    cout << sum << '\n';
+    return 0;
+}
